488c0437c483f5ce727a2d3bba9d31cbf4795598
marcelogomesrp <marcelogomesrp@c75ea249-3d5d-7000-9c21-fc43abfd792c>
2010-11-11 16:45:02 +0000

    Já esta salvando o chamado

--- trunk/AberturaDeChamado/src/java/br/com/guaraba/aberturadechamado/facade/ChamadoController.java
+++ trunk/AberturaDeChamado/src/java/br/com/guaraba/aberturadechamado/facade/ChamadoController.java
@@ -7,6 +7,7 @@ package br.com.guaraba.aberturadechamado.facade;
 
 import br.com.guaraba.aberturadechamado.modelo.Chamado;
 import java.io.Serializable;
+import java.util.Date;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -21,12 +22,29 @@ import javax.faces.bean.SessionScoped;
 public class ChamadoController implements Serializable {
     private Chamado chamado;
     @EJB
-    private ChamadoController chamadoController;
+    private ChamadoFacade chamadoFacade;
 
     public ChamadoController() {
         this.chamado = new Chamado();
     }
 
+    public Chamado getChamado() {
+        return chamado;
+    }
+
+    public void setChamado(Chamado chamado) {
+        this.chamado = chamado;
+    }
+
+    public void NovoChamado(){
+        this.chamado = new Chamado();
+    }
+
+    public void CreateChamado(){
+        this.chamadoFacade.create(chamado);
+        this.NovoChamado();
+        //return "/chamado/novo";
+    }
     
 
 }
